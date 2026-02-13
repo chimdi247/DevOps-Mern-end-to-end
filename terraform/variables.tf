2141a23c6d31334f4100d0e02794869d15396d3f
chimdi247 <Chimdi247@gmail.com>
2026-02-13 14:57:04 +0200

    first commit

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region where resources will be provisioned"
+  default     = "eu-west-2"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  default     = "ami-018ff7ece22bf96db"
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2 instance"
+  default     = "t2.large"
+}
